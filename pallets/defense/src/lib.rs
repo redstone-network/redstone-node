3f8ff8ec10524d3c3fc5df51b650ed38d695d535
shiyivei <shiyivei@outlook.com>
2022-10-30 00:26:36 +0800

    add set_transfer_limit and set_risk_management

--- pallets/defense/src/lib.rs
+++ pallets/defense/src/lib.rs
@@ -33,102 +33,358 @@ pub enum RiskManagement {
 		BoundedVec<u8, ConstU32<256>>, // message body
 	),
 }
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::One;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 	}
 
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
+	/// store transfer limit
 	#[pallet::storage]
-	#[pallet::getter(fn map_transfer_limit)]
+	#[pallet::getter(fn transfer_limit_map)]
 	pub(super) type MapTransferLimit<T: Config> = StorageMap<_, Twox64Concat, u64, TransferLimit>;
 
+	/// store risk management
 	#[pallet::storage]
-	#[pallet::getter(fn map_risk_management)]
+	#[pallet::getter(fn risk_management_map)]
 	pub(super) type MapRiskManagement<T: Config> = StorageMap<_, Twox64Concat, u64, RiskManagement>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
+	/// storage transfer limit owner
+	#[pallet::storage]
+	#[pallet::getter(fn transfer_limit_owner)]
+	pub type TransferLimitOwner<T: Config> =
+		StorageDoubleMap<_, Twox64Concat, T::AccountId, Twox64Concat, u64, (), OptionQuery>;
+
+	/// store risk management owner
+	#[pallet::storage]
+	#[pallet::getter(fn risk_management_owner)]
+	pub type RiskManagementOwner<T: Config> =
+		StorageDoubleMap<_, Twox64Concat, T::AccountId, Twox64Concat, u64, (), OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn next_transfer_limit_id)]
+	pub type NextTransferLimitId<T: Config> = StorageValue<_, u64>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn next_risk_management_id)]
+	pub type NextRiskManagementId<T: Config> = StorageValue<_, u64>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		TransferAmountLimitSet(T::AccountId, TransferLimit),
+		TransferTimesLimitSet(T::AccountId, TransferLimit),
+		TransferAmountLimitUpdated(T::AccountId, TransferLimit),
+		TransferTimesLimitUpdated(T::AccountId, TransferLimit),
+		RiskManagementTimeFreezeSet(T::AccountId, RiskManagement),
+		RiskManagementAccountFreezeSet(T::AccountId, RiskManagement),
+		RiskManagementMailSet(T::AccountId, RiskManagement),
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
 		NoneValue,
-		/// Errors should have helpful documentation associated with them.
 		StorageOverflow,
+		FreezeTimeHasSet,
+		FreezeAccountHasSet,
+		NotifyMailHasSet,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000)]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
 			let who = ensure_signed(origin)?;
 
-			// Update storage.
 			<Something<T>>::put(something);
 
-			// Emit an event.
 			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
+
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000)]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
 			let _who = ensure_signed(origin)?;
 
-			// Read a value from storage.
 			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
 				None => return Err(Error::<T>::NoneValue.into()),
 				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
 					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
+
 					<Something<T>>::put(new);
 					Ok(())
 				},
 			}
 		}
+
+		#[pallet::weight(10_000)]
+		pub fn set_transfer_limit(
+			origin: OriginFor<T>,
+			transfer_limit: TransferLimit,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let transfer_limit_id = NextTransferLimitId::<T>::get().unwrap_or_default();
+
+			log::info!("--------------transfer_limit_id: {}", transfer_limit_id);
+
+			if transfer_limit_id == 0 {
+				MapTransferLimit::<T>::insert(transfer_limit_id, transfer_limit.clone());
+				TransferLimitOwner::<T>::insert(&who, transfer_limit_id, ());
+				NextTransferLimitId::<T>::put(transfer_limit_id.saturating_add(One::one()));
+
+				match transfer_limit {
+					TransferLimit::AmountLimit(_, _) => {
+						log::info!("--------------------------------set transfer amount");
+						Self::deposit_event(Event::TransferAmountLimitSet(
+							who.clone(),
+							transfer_limit,
+						));
+					},
+					TransferLimit::TimesLimit(_, _) => {
+						log::info!("--------------------------------set transfer times");
+						Self::deposit_event(Event::TransferTimesLimitSet(
+							who.clone(),
+							transfer_limit,
+						));
+					},
+				}
+			} else {
+				match transfer_limit {
+					TransferLimit::AmountLimit(_, _) => {
+						let mut transfer_amount_limit_set = false;
+
+						for i in 0..transfer_limit_id {
+							log::info!("{:?}", MapTransferLimit::<T>::get(i));
+							if let Some(TransferLimit::AmountLimit(_set_time, _set_amount)) =
+								MapTransferLimit::<T>::get(i)
+							{
+								log::info!(
+									"--------------------------------update transfer amount"
+								);
+								MapTransferLimit::<T>::insert(i, transfer_limit.clone());
+								MapTransferLimit::<T>::mutate(&i, |v| {
+									*v = Some(transfer_limit.clone())
+								});
+								log::info!("change transfer amount limit successfully");
+								transfer_amount_limit_set = true;
+								Self::deposit_event(Event::TransferAmountLimitUpdated(
+									who.clone(),
+									transfer_limit,
+								));
+								break;
+							}
+						}
+
+						if transfer_amount_limit_set == false {
+							log::info!("--------------------------------set transfer amount");
+							MapTransferLimit::<T>::insert(
+								transfer_limit_id,
+								transfer_limit.clone(),
+							);
+							TransferLimitOwner::<T>::insert(&who, transfer_limit_id, ());
+							NextTransferLimitId::<T>::put(
+								transfer_limit_id.saturating_add(One::one()),
+							);
+
+							Self::deposit_event(Event::TransferAmountLimitSet(
+								who.clone(),
+								transfer_limit,
+							));
+						}
+					},
+
+					TransferLimit::TimesLimit(_, _) => {
+						let mut transfer_times_limit_set = false;
+
+						for i in 0..transfer_limit_id {
+							log::info!("{:?}", MapTransferLimit::<T>::get(i));
+							if let Some(TransferLimit::TimesLimit(_set_time, _set_times)) =
+								MapTransferLimit::<T>::get(i)
+							{
+								log::info!("--------------------------------update transfer times");
+								MapTransferLimit::<T>::insert(i, transfer_limit.clone());
+								MapTransferLimit::<T>::mutate(&i, |v| {
+									*v = Some(transfer_limit.clone())
+								});
+								log::info!("change transfer times limit successfully");
+								transfer_times_limit_set = true;
+								Self::deposit_event(Event::TransferTimesLimitUpdated(
+									who.clone(),
+									transfer_limit,
+								));
+								break;
+							}
+						}
+
+						if transfer_times_limit_set == false {
+							log::info!("--------------------------------set transfer times");
+							MapTransferLimit::<T>::insert(
+								transfer_limit_id,
+								transfer_limit.clone(),
+							);
+							TransferLimitOwner::<T>::insert(&who, transfer_limit_id, ());
+							NextTransferLimitId::<T>::put(
+								transfer_limit_id.saturating_add(One::one()),
+							);
+
+							Self::deposit_event(Event::TransferTimesLimitSet(
+								who.clone(),
+								transfer_limit,
+							));
+						}
+					},
+				}
+			}
+
+			Ok(())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn set_risk_management(
+			origin: OriginFor<T>,
+			risk_management: RiskManagement,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			let risk_management_id = NextRiskManagementId::<T>::get().unwrap_or_default();
+
+			log::info!("--------------risk_management_id: {}", risk_management_id);
+
+			if risk_management_id == 0 {
+				MapRiskManagement::<T>::insert(risk_management_id, risk_management.clone());
+				RiskManagementOwner::<T>::insert(&who, risk_management_id, ());
+				NextRiskManagementId::<T>::put(risk_management_id.saturating_add(One::one()));
+
+				match risk_management {
+					RiskManagement::TimeFreeze(_, _) => {
+						log::info!("--------------------------------set freeze time");
+						Self::deposit_event(Event::RiskManagementTimeFreezeSet(
+							who.clone(),
+							risk_management.clone(),
+						));
+					},
+					RiskManagement::AccountFreeze(_) => {
+						log::info!("--------------------------------set freeze account");
+						Self::deposit_event(Event::RiskManagementAccountFreezeSet(
+							who.clone(),
+							risk_management.clone(),
+						));
+					},
+					RiskManagement::Mail(_, _, _) => {
+						log::info!("--------------------------------set notify mail");
+						Self::deposit_event(Event::RiskManagementMailSet(
+							who.clone(),
+							risk_management.clone(),
+						));
+					},
+				}
+			} else {
+				let mut freeze_time_set = false;
+				let mut freeze_account_set = false;
+				let mut notify_mail_set = false;
+
+				for i in 0..risk_management_id {
+					match MapRiskManagement::<T>::get(i) {
+						Some(RiskManagement::TimeFreeze(_, _)) => {
+							freeze_time_set = true;
+						},
+						Some(RiskManagement::AccountFreeze(_)) => {
+							freeze_account_set = true;
+						},
+						Some(RiskManagement::Mail(_, _, _)) => {
+							notify_mail_set = true;
+						},
+						None => {
+							log::info!("--------------------------------get nothing");
+						},
+					}
+
+					match risk_management {
+						RiskManagement::TimeFreeze(_, _) => {
+							if freeze_time_set == true {
+								Error::<T>::FreezeTimeHasSet;
+							} else {
+								MapRiskManagement::<T>::insert(
+									risk_management_id,
+									risk_management.clone(),
+								);
+								RiskManagementOwner::<T>::insert(&who, risk_management_id, ());
+								NextRiskManagementId::<T>::put(
+									risk_management_id.saturating_add(One::one()),
+								);
+
+								Self::deposit_event(Event::RiskManagementTimeFreezeSet(
+									who.clone(),
+									risk_management.clone(),
+								));
+							}
+						},
+						RiskManagement::AccountFreeze(_) => {
+							if freeze_account_set == true {
+								Error::<T>::FreezeAccountHasSet;
+							} else {
+								MapRiskManagement::<T>::insert(
+									risk_management_id,
+									risk_management.clone(),
+								);
+								RiskManagementOwner::<T>::insert(&who, risk_management_id, ());
+								NextRiskManagementId::<T>::put(
+									risk_management_id.saturating_add(One::one()),
+								);
+
+								Self::deposit_event(Event::RiskManagementAccountFreezeSet(
+									who.clone(),
+									risk_management.clone(),
+								));
+							}
+						},
+						RiskManagement::Mail(_, _, _) => {
+							if notify_mail_set == true {
+								Error::<T>::NotifyMailHasSet;
+							} else {
+								MapRiskManagement::<T>::insert(
+									risk_management_id,
+									risk_management.clone(),
+								);
+								RiskManagementOwner::<T>::insert(&who, risk_management_id, ());
+								NextRiskManagementId::<T>::put(
+									risk_management_id.saturating_add(One::one()),
+								);
+								Self::deposit_event(Event::RiskManagementMailSet(
+									who.clone(),
+									risk_management.clone(),
+								));
+							}
+						},
+					}
+				}
+			}
+
+			Ok(())
+		}
 	}
+
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn offchain_worker(block_number: T::BlockNumber) {
-			log::info!("Hello World from offchain workers!: {:?}", block_number);
+			log::info!("Hi World from defense-pallet workers!: {:?}", block_number);
 		}
 	}
 }
