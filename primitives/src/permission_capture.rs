4b5ff68a4b0ccf7a911cf6a64bb51fa6d877507c
Li Smith <jinrishouji@sina.com>
2022-11-08 18:46:27 +0800

    update PermissionCaptureInterface

---
+++ primitives/src/permission_capture.rs
@@ -0,0 +1,10 @@
+pub trait PermissionCaptureInterface<AccountId, OpaqueCall, Balance> {
+	fn is_account_permission_taken(account: AccountId) -> bool;
+	fn has_account_pedding_call(account: AccountId) -> bool;
+	fn add_call_to_approval_list(
+		account: AccountId,
+		call_hash: [u8; 32],
+		data: OpaqueCall,
+		other_deposit: Balance,
+	) -> bool;
+}
